d52a0097aac7e78fba80040c6136954f1e933d43
Michael Recachinas <m.recachinas@gmail.com>
2022-02-20 15:55:23 -0500

    Change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'mrecachinas.github.io',
+    title: 'Michael Recachinas',
     htmlAttrs: {
       lang: 'en',
     },
