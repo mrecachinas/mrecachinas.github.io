170570018e66fefbd14918f78180a0b56f2f9f8a
Michael Recachinas <m.recachinas@gmail.com>
2022-02-20 15:42:39 -0500

    Refactor socialicons.vue and add CNAME

--- components/SocialIcons.vue
+++ components/SocialIcons.vue
@@ -1,20 +1,8 @@
 <template>
   <div>
     <div class="flex justify-center pt-4 space-x-2">
-      <a href="https://github.com/mrecachinas" target="_blank"
-        ><font-awesome-icon :icon="['fab', 'github']"
-      /></a>
-      <a href="https://instagram.com/mikerecachinas" target="_blank"
-        ><font-awesome-icon :icon="['fab', 'instagram']"
-      /></a>
-      <a href="https://linkedin.com/in/mrecachinas" target="_blank"
-        ><font-awesome-icon :icon="['fa', 'envelope']"
-      /></a>
-      <a href="https://linkedin.com/in/mrecachinas" target="_blank"
-        ><font-awesome-icon :icon="['fab', 'linkedin']"
-      /></a>
-      <a href="https://twitter.com/mikerecachinas" target="_blank"
-        ><font-awesome-icon :icon="['fab', 'twitter']"
+      <a v-for="link in links" :key="link.url" :href="link.url" target="_blank"
+        ><font-awesome-icon :icon="link.icon"
       /></a>
     </div>
   </div>
@@ -23,6 +11,20 @@
 <script>
 export default {
   name: 'SocialIcons',
+  data() {
+    return {
+      links: [
+        { url: 'https://github.com/mrecachinas', icon: ['fab', 'github'] },
+        {
+          url: 'https://instagram.com/mikerecachinas',
+          icon: ['fab', 'instagram'],
+        },
+        { url: 'mailto:m.recachinas@gmail.com', icon: ['fa', 'envelope'] },
+        { url: 'https://github.com/mrecachinas', icon: ['fab', 'linkedin'] },
+        { url: 'https://twitter.com/mikerecachinas', icon: ['fab', 'twitter'] },
+      ],
+    }
+  },
 }
 </script>
 
